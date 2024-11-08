6a908e0d57ab515c38e8d2e4650e33f8f70c7f88
Mike Chi <michi@CT-14069755.activision.com>
2024-11-08 08:28:53 -0800

    updating project to latest

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,3 @@
-import '@/app/ui/global.css';
-import { inter } from './ui/fonts';
-
 export default function RootLayout({
   children,
 }: {
@@ -8,7 +5,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={ `${inter.className} antialiased`}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
